5fd62b59a00f9117a4cc58c37df6316ce843fc6e
SajeedAninda <sajaninda@gmail.com>
2025-04-26 16:02:58 +0600

    Added the logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import logo from '../../images/tech_gear_logo.png'
+import Image from 'next/image'
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div className=''>
+      <div className='h-[14vh] bg-white w-[1200px] mx-auto flex items-center justify-between'>
+        <div className='logo flex gap-1 items-center'>
+          <Image src={logo} width={50} alt='Tech Gear Logo' />
+          <p className='text-[20px] font-bold'>Tech Gear</p>
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
