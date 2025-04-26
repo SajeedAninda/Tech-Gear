e88cd34bb8f3dd87fdc9c995101f617df97d400b
SajeedAninda <sajaninda@gmail.com>
2025-04-26 17:21:23 +0600

    Created the banner

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Banner from './Banner'
 
 const Homepage = () => {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
     </>
   )
 }
