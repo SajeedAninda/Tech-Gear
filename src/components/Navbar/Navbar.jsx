c2af9397a62303a057c2c8ca92a44cfdebff4050
SajeedAninda <sajaninda@gmail.com>
2025-04-26 16:19:54 +0600

    added custom icons and search bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import logo from '../../images/tech_gear_logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import { GrSearch } from 'react-icons/gr'
+import { IoPerson } from 'react-icons/io5'
+import { RiShoppingCart2Fill } from 'react-icons/ri'
 
 const Navbar = () => {
+  const [showSearch, setShowSearch] = useState(false)
+
+  const handleShowSearch = () => {
+    setShowSearch(!showSearch)
+  }
+
   return (
     <div className=''>
       <div className='h-[14vh] bg-white w-[1200px] mx-auto flex items-center justify-between'>
-        <Link href={"/"} className='logo cursor-pointer flex gap-1 items-center'>
+        <Link
+          href={'/'}
+          className='logo cursor-pointer flex gap-1 items-center'
+        >
           <Image src={logo} width={50} alt='Tech Gear Logo' />
           <p className='text-[20px] font-bold'>Tech Gear</p>
         </Link>
@@ -25,6 +38,22 @@ const Navbar = () => {
           <p className='text-[#111111] text-[18px] font-semibold hover:opacity-65 cursor-pointer'>
             Contact
           </p>
+        </div>
+
+        <div className='customs flex gap-6 items-center'>
+          <div className='flex items-center gap-4'>
+            <input
+              className={`w-[240px] rounded-xl bg-[#E5E5E5] py-2 px-6 ${
+                !showSearch ? 'hidden' : ''
+              }`}
+              type='text'
+              name='search'
+              placeholder='Search for Gears'
+            />
+            <GrSearch onClick={handleShowSearch} className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer' />
+          </div>
+          <IoPerson className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer' />
+          <RiShoppingCart2Fill className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer' />
         </div>
       </div>
     </div>
