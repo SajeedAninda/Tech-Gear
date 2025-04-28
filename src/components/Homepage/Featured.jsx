f34a2a2a1154690121209122be3892b1ec7a6527
SajeedAninda <sajaninda@gmail.com>
2025-04-28 21:54:38 +0600

    Added featured images and created features sections

---
+++ src/components/Homepage/Featured.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <div>
+      <div className='my-20 w-[1200px] mx-auto flex items-center justify-between'>
+        <h1 className='text-[#111111] text-[26px] font-bold'>
+          Featured Categories
+        </h1>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+          <div className='bg-[#E5E5E5] rounded-lg p-6'>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Banner from './Banner'
+import Featured from './Featured'
 
 const Homepage = () => {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Featured></Featured>
     </>
   )
 }
