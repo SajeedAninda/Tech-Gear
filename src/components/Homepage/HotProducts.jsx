42f6149e6211b75bf04808ec2f0f032837f8d973
SajeedAninda <sajaninda@gmail.com>
2025-05-01 20:56:51 +0600

    Completed hot product section

--- src/components/Homepage/HotProducts.jsx
+++ src/components/Homepage/HotProducts.jsx
@@ -1,50 +1,113 @@
-import React from 'react';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
+import Image from 'next/image'
+import React from 'react'
 
 const HotProducts = () => {
-    const products = [
-        {
-          name: 'Sony WH-1000XM5 Headphones',
-          price: 15000,
-          discount: 10,
-          tagline: 'Hot Product',
-          photo:
-            'https://m.media-amazon.com/images/I/51FgbLyCYOL._AC_UF1000,1000_QL80_.jpg',
-          rating: 4.8
-        },
-        {
-          name: 'JBL Flip 6 Portable Speaker',
-          price: 18000,
-          discount: 15,
-          tagline: 'Hot Product',
-          photo:
-            'https://www.startech.com.bd/image/cache/catalog/portable-speaker/Jbl/jbl-flip-6/flip-6-black-001-500x500.webp',
-          rating: 4.7
-        },
-        {
-          name: 'Canon EOS M50 Mark II Camera',
-          price: 49000,
-          discount: 12,
-          tagline: 'Hot Product',
-          photo:
-            'https://media.ldlc.com/r1600/ld/products/00/05/81/63/LD0005816350_1.jpg',
-          rating: 4.5
-        },
-        {
-          name: 'Fitbit Charge 6',
-          price: 19000,
-          discount: 8,
-          tagline: 'Hot Product',
-          photo:
-            'https://diamu.com.bd/wp-content/uploads/2022/11/Fitbit-Charge-4.jpg',
-          rating: 4.2
-        },
-      ]
+  const products = [
+    {
+      name: 'Sony WH-1000XM5 Headphones',
+      price: 15000,
+      discount: 10,
+      tagline: 'Hot Product',
+      photo:
+        'https://m.media-amazon.com/images/I/51FgbLyCYOL._AC_UF1000,1000_QL80_.jpg',
+      rating: 4.8
+    },
+    {
+      name: 'JBL Flip 6 Portable Speaker',
+      price: 18000,
+      discount: 15,
+      tagline: 'Hot Product',
+      photo:
+        'https://www.startech.com.bd/image/cache/catalog/portable-speaker/Jbl/jbl-flip-6/flip-6-black-001-500x500.webp',
+      rating: 4.7
+    },
+    {
+      name: 'Canon EOS M50 Mark II Camera',
+      price: 49000,
+      discount: 12,
+      tagline: 'Hot Product',
+      photo:
+        'https://media.ldlc.com/r1600/ld/products/00/05/81/63/LD0005816350_1.jpg',
+      rating: 4.5
+    },
+    {
+      name: 'Fitbit Charge 6',
+      price: 19000,
+      discount: 8,
+      tagline: 'Hot Product',
+      photo:
+        'https://diamu.com.bd/wp-content/uploads/2022/11/Fitbit-Charge-4.jpg',
+      rating: 4.2
+    }
+  ]
 
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className='my-20 w-[1200px] mx-auto'>
+        <h1 className='text-[#111111] text-[26px] font-bold'>Hot Products</h1>
+
+        <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className='w-[280px] bg-[#E5E5E5] shadow-lg rounded-lg p-4 group cursor-pointer hover:shadow-2xl transition duration-300 relative flex flex-col justify-between'
+            >
+              <div>
+                <div className='bg-gradient-to-r from-[#111111] to-[#747373] w-[60px] h-[40px] rounded-br-2xl text-white absolute top-0 left-0 flex justify-center items-center font-bold'>
+                  {Math.round(product.discount)}%
+                </div>
+
+                <div className='bg-gradient-to-r from-[#111111] to-[#747373] w-[100px] text-[14px] h-[40px] rounded-bl-2xl text-white absolute top-0 right-0 flex text-center justify-center items-center font-semibold'>
+                  {product.tagline}
+                </div>
+
+                <div className='p-4 rounded-lg bg-white group-hover:scale-105 transition duration-300'>
+                  <Image
+                    className='w-full h-[200px] object-contain'
+                    src={product.photo}
+                    width={300}
+                    height={300}
+                    alt={product.name}
+                  />
+                </div>
+
+                <h3 className='text-[#111111] text-[18px] mt-5 text-center font-bold min-h-[48px] line-clamp-2'>
+                  {product.name}
+                </h3>
+
+                <div className='mt-3 flex justify-center gap-3 items-center'>
+                  <p className='text-[#111111] text-[16px] font-semibold'>
+                    ৳ {product.price.toLocaleString()}
+                  </p>
+                  <p className='text-[#555] text-[16px] font-semibold line-through'>
+                    ৳{' '}
+                    {(product.price / (1 - product.discount / 100)).toFixed(0)}
+                  </p>
+                </div>
+
+                <div className='mt-3 flex justify-center'>
+                  <Rating
+                    style={{ maxWidth: 140 }}
+                    value={product.rating}
+                    readOnly
+                  />
+                </div>
+              </div>
+
+              <div className='flex items-center gap-3 mt-4'>
+                <button className='bg-[#111111] text-[16px] flex-1 px-3 py-2 rounded-lg text-white cursor-pointer hover:bg-[#555555] transition duration-200 font-bold'>
+                  Details
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default HotProducts;
+export default HotProducts
