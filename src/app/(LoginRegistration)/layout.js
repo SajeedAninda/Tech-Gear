c6b54c00c6123e0e35840860fa097062cf271dab
SajeedAninda <sajaninda@gmail.com>
2025-05-02 15:28:14 +0600

    Created a design for the login page

---
+++ src/app/(LoginRegistration)/layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginRegistrationLayout = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default LoginRegistrationLayout;
---
+++ src/app/(LoginRegistration)/login/page.js
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import React from 'react';
+import loginImg from "../../../images/loginImg.jpg";
+import logo from "../../../images/tech_gear_logo.png";
+
+const Login = () => {
+    return (
+        <div className='bg-gradient-to-r from-[#111111] to-[#616161]'>
+            <div className='w-[1200px] mx-auto h-screen py-12'>
+                <div className='w-full flex justify-between items-center h-full'>
+                    <div className='ImgDiv w-[50%] relative h-full rounded-l-2xl'>
+                        <Image
+                            src={loginImg}
+                            alt="Login"
+                            fill
+                            className='object-cover rounded-l-2xl'
+                        />
+                    </div>
+
+                    <div className='bg-white w-[50%] px-12 py-8 rounded-r-2xl'>
+                        <Image className='w-[100px]' src={logo} alt='logo'></Image>
+                        <h1 className='text-[#111111] font-bold text-[30px]'>Welcome Back!</h1>
+                        <p className='text-[#111111] mt-3 font-medium text-[16px]'>Please Login to continue browsing and purchase your desired Gear</p>
+                    </div>
+                </div>
+
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
