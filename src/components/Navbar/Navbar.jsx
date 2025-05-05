90995ff91a7178d7af2b29fa19c37196193eddae
SajeedAninda <sajaninda@gmail.com>
2025-05-05 16:23:40 +0600

    Created a dropdown for shop menus

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,33 +8,125 @@ import { IoPerson } from 'react-icons/io5'
 import { RiShoppingCart2Fill } from 'react-icons/ri'
 import useCurrentUser from '../Hooks/useCurrentUser'
 
+const gadgetData = {
+  Smartphones: [
+    'Apple',
+    'Samsung',
+    'Xiaomi',
+    'OnePlus',
+    'Google',
+    'Oppo',
+    'Vivo',
+    'Motorola',
+    'Realme',
+    'Nokia',
+    'Asus',
+    'Sony',
+    'Huawei',
+    'Tecno',
+    'Infinix'
+  ],
+  Laptops: [
+    'Dell',
+    'HP',
+    'Lenovo',
+    'Apple',
+    'Asus',
+    'Acer',
+    'Microsoft',
+    'MSI',
+    'Razer',
+    'Samsung'
+  ],
+  Headphones: [
+    'Sony',
+    'Bose',
+    'Sennheiser',
+    'JBL',
+    'Beats by Dre',
+    'Skullcandy',
+    'Audio-Technica'
+  ],
+  Camera: ['Canon', 'Nikon', 'Sony', 'Fujifilm', 'Panasonic'],
+  Earbuds: [
+    'Apple',
+    'Samsung',
+    'OnePlus',
+    'Sony',
+    'JBL',
+    'Realme',
+    'Oppo',
+    'Nothing',
+    'Bose',
+    'Beats'
+  ],
+  Gaming: [],
+  Speakers: ['JBL', 'Bose', 'Sony', 'Marshall', 'Harman Kardon'],
+  Smartwatches: [
+    'Apple',
+    'Samsung',
+    'Garmin',
+    'Fitbit',
+    'Huawei',
+    'Amazfit',
+    'Fossil'
+  ]
+}
+
 const Navbar = () => {
   const [showSearch, setShowSearch] = useState(false)
-
-  const handleShowSearch = () => {
-    setShowSearch(!showSearch)
-  }
+  const handleShowSearch = () => setShowSearch(!showSearch)
 
   const { userData } = useCurrentUser()
-  console.log(userData)
+
   return (
-    <div>
-      <div className='h-[14vh] bg-white w-[1200px] mx-auto flex items-center justify-between'>
-        <Link
-          href={'/'}
-          className='logo cursor-pointer flex gap-1 items-center'
-        >
+    <div className='bg-white shadow'>
+      <div className='h-[14vh] w-[1200px] mx-auto flex items-center justify-between relative'>
+        <Link href='/' className='logo cursor-pointer flex gap-1 items-center'>
           <Image src={logo} width={50} alt='Tech Gear Logo' />
           <p className='text-[20px] font-bold'>Tech Gear</p>
         </Link>
 
-        <div className='links flex gap-6 items-center'>
+        <div className='links flex gap-6 items-center relative'>
           <p className='text-[#111111] text-[18px] font-semibold hover:opacity-65 cursor-pointer'>
             Home
           </p>
-          <p className='text-[#111111] text-[18px] font-semibold hover:opacity-65 cursor-pointer'>
-            Shop
-          </p>
+
+          <div className='relative'>
+            <p className='text-[#111111] text-[18px] font-semibold hover:opacity-65 cursor-pointer peer'>
+              Shop
+            </p>
+
+            {/* Main Dropdown */}
+            <div className='absolute top-full left-0 mt-2 z-50 opacity-0 invisible peer-hover:visible peer-hover:opacity-100 hover:visible hover:opacity-100 transition duration-300 ease-in-out'>
+              <div className='bg-white shadow-2xl rounded-md w-[300px] flex flex-col gap-4 p-4'>
+                {Object.entries(gadgetData).map(([gadget, brands]) => (
+                  <div key={gadget} className='relative group'>
+                    <p className='font-bold text-[#111] mb-2 whitespace-nowrap cursor-pointer hover:text-blue-600'>
+                      {gadget}
+                    </p>
+
+                    {/* Submenu */}
+                    {brands.length > 0 && (
+                      <div className='absolute left-full top-0 ml-2 bg-white shadow-2xl p-4 rounded-md opacity-0 invisible group-hover:visible group-hover:opacity-100 transition duration-300 z-50 min-w-[250px]'>
+                        <ul className='text-sm space-y-1'>
+                          {brands.map((brand, i) => (
+                            <li
+                              key={i}
+                              className='hover:text-blue-600 cursor-pointer whitespace-nowrap'
+                            >
+                              {brand}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
           <p className='text-[#111111] text-[18px] font-semibold hover:opacity-65 cursor-pointer'>
             About
           </p>
@@ -55,23 +147,28 @@ const Navbar = () => {
               name='search'
               placeholder='Search for Gears'
             />
-
             <GrSearch
               onClick={handleShowSearch}
               className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer relative z-10'
             />
           </div>
+
           <div>
             {userData ? (
-              <div>
-                <Image className='rounded-full w-[30px] cursor-pointer hover:opacity-65' src={userData?.imageUrl} width={20} height={24} alt='User Image'></Image>
-              </div>
+              <Image
+                className='rounded-full w-[30px] h-[30px] cursor-pointer hover:opacity-65'
+                src={userData?.imageUrl}
+                width={30}
+                height={30}
+                alt='User Image'
+              />
             ) : (
-              <Link href={'/login'}>
+              <Link href='/login'>
                 <IoPerson className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer' />
               </Link>
             )}
           </div>
+
           <RiShoppingCart2Fill className='text-[#111111] text-[24px] font-bold hover:opacity-65 cursor-pointer' />
         </div>
       </div>
