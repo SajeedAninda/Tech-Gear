768b7c4cbeb8f42b928b21b591d2b530b2d7dc53
SajeedAninda <sajaninda@gmail.com>
2025-05-07 19:12:48 +0600

    Created boxes for dashboard

--- src/app/(AdminPanel)/adminPanel/page.js
+++ src/app/(AdminPanel)/adminPanel/page.js
@@ -1,9 +1,11 @@
+import Dashboard from '@/components/Admin/Dashboard';
 import React from 'react';
 
 const page = () => {
     return (
-        <div className='bg-white rounded-r-2xl h-full'>
-            Admin Panel
+        <div className='bg-white rounded-r-2xl h-full px-10 py-8'>
+           <h3 className='text-[#111111] text-[30px] font-bold'>Dashboard</h3>
+           <Dashboard></Dashboard>
         </div>
     );
 };
---
+++ src/components/Admin/Dashboard.jsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+import { BsBorderStyle } from 'react-icons/bs'
+import { PiUsersThreeFill } from "react-icons/pi";
+
+
+const Dashboard = () => {
+  return (
+    <div className='w-full mt-4 flex justify-between items-center gap-4'>
+      <div className='flex flex-col gap-4'>
+        <div className='flex justify-between items-center gap-4'>
+          <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
+            <span>Total Products</span>
+            <span className='text-[30px] font-bold'>$ 170</span>
+          </div>
+          <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
+            <span>Total Orders</span>
+            <span className='text-[30px] font-bold flex items-center gap-4'>
+              <BsBorderStyle /> 20
+            </span>
+          </div>
+        </div>
+        <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
+          <span>Total Users</span>
+          <span className='text-[30px] font-bold flex items-center gap-4'>
+            <PiUsersThreeFill />
+            10
+          </span>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Dashboard
