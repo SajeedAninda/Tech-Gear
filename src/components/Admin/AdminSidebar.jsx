465e664adbc1c744798a7cca00b7dde4dcc75137
SajeedAninda <sajaninda@gmail.com>
2025-05-07 17:40:13 +0600

    Designed sidebar links

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
+import { MdOutlineDashboard } from 'react-icons/md'
+import { BsBorderStyle } from "react-icons/bs";
+import { AiFillProduct } from "react-icons/ai";
+import { MdFormatListBulletedAdd } from "react-icons/md";
+import { ImUsers } from "react-icons/im";
+import { IoSettings } from "react-icons/io5";
+
 
 const AdminSidebar = () => {
-    return (
-        <div className='h-full bg-[#111111] rounded-l-2xl'>
-            
-        </div>
-    );
-};
+  return (
+    <div className='h-full bg-[#111111] rounded-l-2xl py-10 px-8'>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2'>
+        <MdOutlineDashboard />
+        Dashboard
+      </Link>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2 '>
+        <BsBorderStyle />
+        Orders
+      </Link>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2 '>
+        <AiFillProduct />
+        Products
+      </Link>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2 '>
+        <MdFormatListBulletedAdd />
+        Add Products
+      </Link>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2 '>
+        <ImUsers />
+        Users
+      </Link>
+      <Link href={'/'} className='text-white p-4 rounded-xl hover:bg-gray-800 transition-all duration-300 text-[20px] font-semibold flex items-center gap-2 '>
+        <IoSettings />
+        Settings
+      </Link>
+    </div>
+  )
+}
 
-export default AdminSidebar;
+export default AdminSidebar
