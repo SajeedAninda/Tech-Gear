26b8a29089072eb579a834a2f8f4d5ae343fe76a
SajeedAninda <sajaninda@gmail.com>
2025-05-09 18:44:30 +0600

    Minor changes

--- src/components/Admin/AddProduct.jsx
+++ src/components/Admin/AddProduct.jsx
@@ -175,12 +175,29 @@ const AddProduct = () => {
               placeholder='Discount %'
               className='border py-2 px-3 rounded-lg placeholder:text-[#111111]'
             />
-            <input
-              type='text'
+            <select
               name='tagline'
-              placeholder='Product Tagline'
-              className='border py-2 px-3 rounded-lg placeholder:text-[#111111]'
-            />
+              className='border py-2 px-3 rounded-lg text-[#111111] bg-white transition duration-300 focus:ring-2 focus:ring-[#111111]'
+              required
+            >
+              <option value=''>Choose Product Tagline</option>
+              <option value='latest' className='bg-[#111111] text-white'>
+                Latest
+              </option>
+              <option value='best-seller' className='bg-[#111111] text-white'>
+                Best Seller
+              </option>
+              <option value='most-popular' className='bg-[#111111] text-white'>
+                Most Popular
+              </option>
+              <option value='new-comer' className='bg-[#111111] text-white'>
+                New Comer
+              </option>
+              <option value='hot-product' className='bg-[#111111] text-white'>
+                Hot Product
+              </option>
+            </select>
+
             <input
               type='number'
               name='rating'
@@ -195,9 +212,19 @@ const AddProduct = () => {
 
         <button
           type='submit'
-          className='bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800'
+          className='flex justify-center hover:bg-[#2e2e2e] w-full cursor-pointer px-10 py-2 mt-10 gap-2 items-center shadow-xl text-lg text-white hover:text-gray-300 bg-[#111111]  backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10  overflow-hidden border-2 rounded-full group'
         >
           Add Product
+          <svg
+            className='w-8 h-8  group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45'
+            viewBox='0 0 16 19'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z'
+              className='fill-gray-800 group-hover:fill-gray-800'
+            ></path>
+          </svg>
         </button>
       </form>
     </div>
