1f081d651f731ac547777b8342f8268951fed7a3
SajeedAninda <sajaninda@gmail.com>
2025-05-11 15:49:59 +0600

    Created all products heading

--- src/app/(DefaultLayout)/shop/page.js
+++ src/app/(DefaultLayout)/shop/page.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            Shopping
+        <div className='my-12 w-[1200px] mx-auto'>
+            <h3 className='text-[#111111] text-[30px] font-bold'>Shop For Tech Products!</h3>
+            <h4 className='text-[#555555] text-[18px] font-medium'>Explore Our Wide Range of the Latest Gadgets and Innovations</h4>
         </div>
     );
 };
