df7003eb132f728ccd993050b7db6f15d8f9ac18
SajeedAninda <sajaninda@gmail.com>
2025-05-19 16:49:01 +0600

    Fetched product details for a certain individual product

--- src/components/Shopping/ProductDetails/ProductDetails.jsx
+++ src/components/Shopping/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,6 @@
 'use client'
 import useAxiosInstance from '@/components/Hooks/useAxiosInstance';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import React from 'react';
 
@@ -8,6 +9,20 @@ const ProductDetails = () => {
   const params = useParams()
   const id = params?.id
 
+  const {
+    data: productDetails,
+    isLoading: isProductLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['productDetails', id],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/productDetails/${id}`)
+      return response.data
+    },
+    enabled: !!id
+  })
+
+  console.log(productDetails);
 
     return (
         <div>
