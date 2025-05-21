001dc6a67d6863947d3da2f2ad007dcfa8950af6
SajeedAninda <sajaninda@gmail.com>
2025-05-21 18:07:50 +0600

    Placed the product count

--- src/components/Admin/Dashboard.jsx
+++ src/components/Admin/Dashboard.jsx
@@ -1,17 +1,22 @@
 'use client'
 import React from 'react'
 import { BsBorderStyle } from 'react-icons/bs'
-import { PiUsersThreeFill } from "react-icons/pi";
-
+import { PiUsersThreeFill } from 'react-icons/pi'
+import useAllProducts from '../Hooks/useAllProducts'
 
 const Dashboard = () => {
+  let { products } = useAllProducts()
+
+  let productsCount = products?.length
+
+
   return (
     <div className='w-full mt-4 flex justify-between items-center gap-4'>
       <div className='flex flex-col gap-4'>
         <div className='flex justify-between items-center gap-4'>
           <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
             <span>Total Products</span>
-            <span className='text-[30px] font-bold'>$ 170</span>
+            <span className='text-[30px] font-bold'>{productsCount}</span>
           </div>
           <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
             <span>Total Orders</span>
