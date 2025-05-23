c0e2b38b59b5aba25bab2361aedd287e5633ea17
SajeedAninda <sajaninda@gmail.com>
2025-05-23 17:21:04 +0600

    Minor change

--- src/app/(AdminPanel)/products/page.js
+++ src/app/(AdminPanel)/products/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='bg-white rounded-r-2xl px-10 py-8 h-fit'>
+        <div className='bg-white rounded-r-2xl px-10 py-8 h-full'>
             <h3 className='text-[#111111] text-[30px] font-bold'>Add Product</h3>
             <Products></Products>
         </div>
--- src/app/(AdminPanel)/users/page.js
+++ src/app/(AdminPanel)/users/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='bg-white rounded-r-2xl px-10 py-8 h-fit'>
+        <div className='bg-white rounded-r-2xl px-10 py-8 h-full'>
             <h3 className='text-[#111111] text-[30px] font-bold'>All Users</h3>
             <Users></Users>
         </div>
