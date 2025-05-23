020f3db5d89758e85f5a6292c53047d956e85712
SajeedAninda <sajaninda@gmail.com>
2025-05-23 14:53:53 +0600

    Fetched all users

--- src/app/(AdminPanel)/users/page.js
+++ src/app/(AdminPanel)/users/page.js
@@ -1,10 +1,11 @@
+import Users from '@/components/Admin/Users';
 import React from 'react';
 
 const page = () => {
     return (
         <div className='bg-white rounded-r-2xl px-10 py-8 h-fit'>
             <h3 className='text-[#111111] text-[30px] font-bold'>All Users</h3>
-            {/* <Products></Products> */}
+            <Users></Users>
         </div>
     );
 };
--- src/components/Admin/Users.jsx
+++ src/components/Admin/Users.jsx
@@ -1,6 +1,26 @@
+'use client'
 import React from 'react';
+import useAxiosInstance from '../Hooks/useAxiosInstance';
+import { useQuery } from '@tanstack/react-query';
 
 const Users = () => {
+    let axiosInstance = useAxiosInstance()
+
+  const {
+    data: userData,
+    isLoading: isUsersLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/allUsers`)
+      return response.data
+    }
+  })
+
+  console.log(userData);
+
+
     return (
         <div>
             
