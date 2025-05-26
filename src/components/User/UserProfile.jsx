0fcf118dc929a1a79a7e0d5833d6d9cd67336700
SajeedAninda <sajaninda@gmail.com>
2025-05-26 17:17:48 +0600

    Fetched cart data based on user

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -1,9 +1,26 @@
 'use client'
 import React from 'react'
 import useCurrentUser from '../Hooks/useCurrentUser'
+import useAuth from '../Hooks/useAuth'
+import useAxiosInstance from '../Hooks/useAxiosInstance'
+import { useQuery } from '@tanstack/react-query'
 
 const UserProfile = () => {
   const { userData, isUserLoading } = useCurrentUser()
+  let { loggedInUser } = useAuth()
+  let currentUserEmail = loggedInUser?.email
+  let axiosInstance = useAxiosInstance()
+
+  const { data: cartData, isLoading: isCartLoading } = useQuery({
+    queryKey: ['cartData', currentUserEmail],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/getCart/${currentUserEmail}`)
+      return response.data
+    },
+    enabled: !!currentUserEmail
+  })
+
+  console.log(cartData)
 
   return (
     <div>
