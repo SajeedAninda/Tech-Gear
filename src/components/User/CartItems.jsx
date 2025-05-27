c18b12866daa6e97f326978baafa08f375fcb54c
SajeedAninda <sajaninda@gmail.com>
2025-05-27 16:55:39 +0600

    Added cart table

--- src/components/User/CartItems.jsx
+++ src/components/User/CartItems.jsx
@@ -1,12 +1,15 @@
 'use client'
-import React from 'react';
-import useCurrentUser from '../Hooks/useCurrentUser';
-import useAuth from '../Hooks/useAuth';
-import useAxiosInstance from '../Hooks/useAxiosInstance';
-import { useQuery } from '@tanstack/react-query';
+import React from 'react'
+import useCurrentUser from '../Hooks/useCurrentUser'
+import useAuth from '../Hooks/useAuth'
+import useAxiosInstance from '../Hooks/useAxiosInstance'
+import { useQuery } from '@tanstack/react-query'
+import { RiDeleteBinFill } from 'react-icons/ri'
+import { MdEditSquare } from 'react-icons/md'
+import Link from 'next/link'
 
 const CartItems = () => {
-    const { userData, isUserLoading } = useCurrentUser()
+  const { userData, isUserLoading } = useCurrentUser()
   let { loggedInUser } = useAuth()
   let currentUserEmail = loggedInUser?.email
   let axiosInstance = useAxiosInstance()
@@ -19,12 +22,85 @@ const CartItems = () => {
     },
     enabled: !!currentUserEmail
   })
-  
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default CartItems;
+  return (
+    <div>
+      <div className='mt-8'>
+        <h1 className='text-[22px] text-[#111111] font-bold'>
+          Total Products in Cart: {cartData?.length}
+        </h1>
+      </div>
+
+      <div className='mt-4'>
+        <div className='bg-gradient-to-r from-[#111111]  to-[#747373] rounded-tl-xl rounded-tr-xl grid grid-cols-12 px-2 md:px-6 py-4'>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+            #SL
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+            Image
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
+            Name
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
+            Brand
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>
+            Price
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+            Update
+          </div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+            Delete
+          </div>
+        </div>
+      </div>
+
+      {cartData ? (
+        cartData?.map((product, index) => (
+          <div key={index} className=''>
+            <div className='bg-[#F7FFF7] border-b-2 border-[#111111] grid grid-cols-12 px-2 md:px-6 py-4 items-center'>
+              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+                {index + 1}
+              </div>
+              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'>
+                <img
+                  className='w-[50px] h-[50px] rounded-full object-cover'
+                  src={product?.productImages[0]}
+                  alt=''
+                />
+              </div>
+              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
+                {product?.name}
+              </div>
+              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
+                {product?.brand}
+              </div>
+              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-2 text-center capitalize'>
+                ৳ {product?.price}
+              </div>
+
+              <Link
+                href={`products/${product?._id}`}
+                className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'
+              >
+                <MdEditSquare className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#111111] hover:opacity-60' />
+              </Link>
+
+              <div
+                className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'
+              >
+                <RiDeleteBinFill className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#ed4747] hover:opacity-60' />
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>Loading....</div>
+      )}
+    </div>
+  )
+}
+
+export default CartItems
