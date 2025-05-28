9953acb2d83af068a9438651ecc45f77ee8469b8
SajeedAninda <sajaninda@gmail.com>
2025-05-28 16:47:02 +0600

    Change product quantity with counter

--- src/components/User/CartItems.jsx
+++ src/components/User/CartItems.jsx
@@ -1,20 +1,20 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import useCurrentUser from '../Hooks/useCurrentUser'
 import useAuth from '../Hooks/useAuth'
 import useAxiosInstance from '../Hooks/useAxiosInstance'
 import { useQuery } from '@tanstack/react-query'
 import { RiDeleteBinFill } from 'react-icons/ri'
-import { MdEditSquare } from 'react-icons/md'
+import { FaMinus, FaPlus } from 'react-icons/fa'
 import Link from 'next/link'
 import Swal from 'sweetalert2'
 import toast from 'react-hot-toast'
 
 const CartItems = () => {
   const { userData, isUserLoading } = useCurrentUser()
-  let { loggedInUser } = useAuth()
-  let currentUserEmail = loggedInUser?.email
-  let axiosInstance = useAxiosInstance()
+  const { loggedInUser } = useAuth()
+  const currentUserEmail = loggedInUser?.email
+  const axiosInstance = useAxiosInstance()
 
   const {
     data: cartData,
@@ -29,7 +29,21 @@ const CartItems = () => {
     enabled: !!currentUserEmail
   })
 
-  let handleDeleteProduct = id => {
+  const [quantities, setQuantities] = useState({})
+
+  const handleQuantityChange = (productId, delta) => {
+    setQuantities(prev => {
+      const currentQty = prev[productId] ?? (cartData.find(p => p._id === productId)?.productQuantity ?? 1)
+      const newQty = Math.min(10, Math.max(1, currentQty + delta))
+      console.log(`Product ID: ${productId}, New Quantity: ${newQty}`)
+      return {
+        ...prev,
+        [productId]: newQty
+      }
+    })
+  }
+
+  const handleDeleteProduct = id => {
     Swal.fire({
       title: 'Do You want to Remove this from Cart?',
       text: 'Once Deleted, you cannot revert this!',
@@ -43,16 +57,14 @@ const CartItems = () => {
         axiosInstance
           .delete(`/deleteCartProduct/${id}`)
           .then(res => {
-            console.log(res.data)
             if (res.data.deletedCount > 0) {
               refetch()
-              console.log(res.data)
               toast.success('Removed from Cart')
             }
           })
           .catch(error => {
-            console.error('Error :', error)
-            toast.error('Error', 'Failed to remove from Cart')
+            console.error('Error:', error)
+            toast.error('Failed to remove from Cart')
           })
       }
     })
@@ -62,80 +74,104 @@ const CartItems = () => {
     <div>
       <div className='mt-8'>
         <h1 className='text-[22px] text-[#111111] font-bold'>
-          Total Products in Cart: {cartData?.length}
+          Total Products in Cart: {cartData?.length || 0}
         </h1>
       </div>
 
       <div className='mt-4'>
-        <div className='bg-gradient-to-r from-[#111111]  to-[#747373] rounded-tl-xl rounded-tr-xl grid grid-cols-12 px-2 md:px-6 py-4'>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
-            #SL
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
-            Image
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
-            Name
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
-            Brand
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>
-            Price
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
-            Quantity
-          </div>
-          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
-            Delete
-          </div>
+        <div className='bg-gradient-to-r from-[#111111] to-[#747373] rounded-tl-xl rounded-tr-xl grid grid-cols-12 px-2 md:px-6 py-4'>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>#SL</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>Image</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>Name</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>Brand</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>Price</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>Quantity</div>
+          <div className='text-white font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>Delete</div>
         </div>
       </div>
 
       {cartData ? (
-        cartData?.map((product, index) => (
-          <div key={index} className=''>
-            <Link
-              href={`shop/product/${product?.productId}`}
-              className='bg-[#F7FFF7] border-b-2 border-[#111111] grid grid-cols-12 px-2 md:px-6 py-4 items-center cursor-pointer hover:bg-gray-300 transition-all duration-150'
-            >
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
-                {index + 1}
-              </div>
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'>
-                <img
-                  className='w-[50px] h-[50px] rounded-full object-cover'
-                  src={product?.productImages[0]}
-                  alt=''
-                />
-              </div>
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
-                {product?.name}
-              </div>
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
-                {product?.brand}
-              </div>
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-2 text-center capitalize'>
-                ৳ {product?.price}
-              </div>
+        cartData.map((product, index) => {
+          const productQty = quantities[product._id] ?? product.productQuantity
 
-              <div className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center capitalize'>
-                {product?.productQuantity}
-              </div>
-
-              <div
-                onClick={e => {
-                  e.stopPropagation()
-                  e.preventDefault()
-                  handleDeleteProduct(product?._id)
-                }}
-                className='text-[#111111] font-bold  text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'
+          return (
+            <div key={product._id}>
+              <Link
+                href={`shop/product/${product.productId}`}
+                className='bg-[#F7FFF7] border-b-2 border-[#111111] grid grid-cols-12 px-2 md:px-6 py-4 items-center cursor-pointer hover:bg-gray-300 transition-all duration-150'
               >
-                <RiDeleteBinFill className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#ed4747] hover:opacity-60' />
-              </div>
-            </Link>
-          </div>
-        ))
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center'>
+                  {index + 1}
+                </div>
+
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'>
+                  <img
+                    className='w-[50px] h-[50px] rounded-full object-cover'
+                    src={product?.productImages[0]}
+                    alt=''
+                  />
+                </div>
+
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-3 text-center'>
+                  {product?.name}
+                </div>
+
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center'>
+                  {product?.brand}
+                </div>
+
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center capitalize'>
+                  ৳ {product?.price}
+                </div>
+
+                <div className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-2 text-center capitalize'>
+                  <div className='flex items-center gap-3 justify-center'>
+                    <button
+                      onClick={e => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        handleQuantityChange(product._id, -1)
+                      }}
+                      disabled={productQty <= 1}
+                      className={`bg-[#111111] text-white p-1 rounded-lg font-bold hover:bg-[#555555] transition duration-200 ${
+                        productQty <= 1 ? 'opacity-50 cursor-not-allowed' : ''
+                      }`}
+                    >
+                      <FaMinus />
+                    </button>
+
+                    <span className='text-[20px] font-semibold'>{productQty}</span>
+
+                    <button
+                      onClick={e => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        handleQuantityChange(product._id, 1)
+                      }}
+                      disabled={productQty >= 10}
+                      className={`bg-[#111111] text-white p-1 rounded-lg font-bold hover:bg-[#555555] transition duration-200 ${
+                        productQty >= 10 ? 'opacity-50 cursor-not-allowed' : ''
+                      }`}
+                    >
+                      <FaPlus />
+                    </button>
+                  </div>
+                </div>
+
+                <div
+                  onClick={e => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    handleDeleteProduct(product._id)
+                  }}
+                  className='text-[#111111] font-bold text-[9px] md:text-base lg:text-[18px] col-span-1 text-center flex justify-center'
+                >
+                  <RiDeleteBinFill className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#ed4747] hover:opacity-60' />
+                </div>
+              </Link>
+            </div>
+          )
+        })
       ) : (
         <div>Loading....</div>
       )}
