ac51f5826b2a9baad4b0e424ad16196c276ab106
SajeedAninda <sajaninda@gmail.com>
2025-05-30 16:43:35 +0600

    Created Cart Provider to get cart values across components

---
+++ src/components/Provider/CartProvider.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext } from 'react'
+import useAuth from '../Hooks/useAuth'
+import useAxiosInstance from '../Hooks/useAxiosInstance'
+import { useQuery } from '@tanstack/react-query'
+
+const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+  const { loggedInUser } = useAuth()
+  const currentUserEmail = loggedInUser?.email
+  const axiosInstance = useAxiosInstance()
+
+  const {
+    data: cartData,
+    isLoading: isCartLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['cartData', currentUserEmail],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/getCart/${currentUserEmail}`)
+      return response.data
+    },
+    enabled: !!currentUserEmail
+  })
+
+  return (
+    <CartContext.Provider value={{ cartData, isCartLoading, refetch }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => useContext(CartContext)
