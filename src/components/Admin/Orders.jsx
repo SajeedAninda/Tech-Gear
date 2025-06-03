2dd9e7d7fe4d45777b341ad615aa3dbafa1397a3
SajeedAninda <sajaninda@gmail.com>
2025-06-03 14:38:45 +0600

    Fetched all orders

--- src/components/Admin/Orders.jsx
+++ src/components/Admin/Orders.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import useAxiosInstance from '../Hooks/useAxiosInstance';
+import { useQuery } from '@tanstack/react-query';
 
 const Orders = () => {
-    
+    let axiosInstance = useAxiosInstance()
+  const {
+    data: orders,
+    isLoading: isOrdersLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['orders'],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/allOrders`)
+      return response.data
+    }
+  })
 
     return (
         <div>
