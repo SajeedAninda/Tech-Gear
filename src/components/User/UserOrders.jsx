4b98dca1b98511451f8f8f9e1ab23039f8c176f5
SajeedAninda <sajaninda@gmail.com>
2025-06-04 14:54:38 +0600

    Fetched order data for user

--- src/components/User/UserOrders.jsx
+++ src/components/User/UserOrders.jsx
@@ -1,6 +1,30 @@
+'use client'
 import React from 'react';
+import useAuth from '../Hooks/useAuth';
+import useAxiosInstance from '../Hooks/useAxiosInstance';
+import { useQuery } from '@tanstack/react-query';
 
 const UserOrders = () => {
+     const { loggedInUser } = useAuth()
+  const currentUserEmail = loggedInUser?.email
+  const axiosInstance = useAxiosInstance()
+
+   const {
+    data: userOrders,
+    isLoading: isOrdersLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['userOrders', currentUserEmail],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/userOrders/${currentUserEmail}`)
+      return response.data
+    },
+    enabled: !!currentUserEmail
+  })
+
+  console.log(userOrders);
+
+
     return (
         <div>
             
