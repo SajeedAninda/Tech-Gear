0cd50b0c605d84865b7e1a9302902adab459d92a
SajeedAninda <sajaninda@gmail.com>
2025-06-08 14:55:54 +0600

    Sorted hot poducts section

--- src/components/Homepage/HotProducts.jsx
+++ src/components/Homepage/HotProducts.jsx
@@ -6,11 +6,12 @@ import '@smastrom/react-rating/style.css'
 import Image from 'next/image'
 import React from 'react'
 import useAllProducts from '../Hooks/useAllProducts'
+import Link from 'next/link'
 
 const HotProducts = () => {
   const { products = [], isProductsLoading, refetch } = useAllProducts()
 
-  const hotProducts = products.filter(
+  const hotProducts = products?.filter(
     product => product.tagline === 'hot-product'
   )
 
@@ -24,58 +25,61 @@ const HotProducts = () => {
         <h1 className='text-[#111111] text-[26px] font-bold'>Hot Products</h1>
 
         <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
-          {shuffledHotProducts.map((product, index) => (
+          {shuffledHotProducts?.map((product, index) => (
             <div
               key={index}
               className='w-[280px] bg-[#E5E5E5] shadow-lg rounded-lg p-4 group cursor-pointer hover:shadow-2xl transition duration-300 relative flex flex-col justify-between'
             >
               <div>
                 <div className='bg-gradient-to-r from-[#111111] to-[#747373] w-[60px] h-[40px] rounded-br-2xl text-white absolute top-0 left-0 flex justify-center items-center font-bold'>
-                  {Math.round(product.discount)}%
+                  {Math.round(product?.discount)}%
                 </div>
 
                 <div className='bg-gradient-to-r from-[#111111] to-[#747373] w-[100px] text-[14px] h-[40px] rounded-bl-2xl text-white absolute top-0 right-0 flex text-center justify-center items-center font-semibold'>
-                  {product.tagline}
+                  {product?.tagline}
                 </div>
 
                 <div className='p-4 rounded-lg bg-white group-hover:scale-105 transition duration-300'>
                   <Image
                     className='w-full h-[200px] object-contain'
-                    src={product?.photo}
+                    src={product?.productImages?.[0]}
                     width={300}
                     height={300}
-                    alt={product.name}
+                    alt={product?.name}
                   />
                 </div>
 
                 <h3 className='text-[#111111] text-[18px] mt-5 text-center font-bold min-h-[48px] line-clamp-2'>
-                  {product.name}
+                  {product?.name}
                 </h3>
 
                 <div className='mt-3 flex justify-center gap-3 items-center'>
                   <p className='text-[#111111] text-[16px] font-semibold'>
-                    ৳ {product.price.toLocaleString()}
+                    ৳ {product?.price.toLocaleString()}
                   </p>
                   <p className='text-[#555] text-[16px] font-semibold line-through'>
                     ৳{' '}
-                    {(product.price / (1 - product.discount / 100)).toFixed(0)}
+                    {(product?.price / (1 - product.discount / 100)).toFixed(0)}
                   </p>
                 </div>
 
                 <div className='mt-3 flex justify-center'>
                   <Rating
                     style={{ maxWidth: 140 }}
-                    value={product.rating}
+                    value={product?.rating}
                     readOnly
                   />
                 </div>
               </div>
 
-              <div className='flex items-center gap-3 mt-4'>
+              <Link
+                href={`/shop/product/${product?._id}`}
+                className='flex items-center gap-3 mt-4'
+              >
                 <button className='bg-[#111111] text-[16px] flex-1 px-3 py-2 rounded-lg text-white cursor-pointer hover:bg-[#555555] transition duration-200 font-bold'>
                   Details
                 </button>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
