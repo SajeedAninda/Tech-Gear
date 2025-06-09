f2b3874ccbbe2741bcdeadb89e6dedb94c4b32b0
SajeedAninda <sajaninda@gmail.com>
2025-06-09 16:56:48 +0600

    Added total order amount in user dashboard

--- src/components/Admin/Dashboard.jsx
+++ src/components/Admin/Dashboard.jsx
@@ -4,11 +4,21 @@ import { BsBorderStyle } from 'react-icons/bs'
 import { PiUsersThreeFill } from 'react-icons/pi'
 import useAllProducts from '../Hooks/useAllProducts'
 import { AiFillProduct } from 'react-icons/ai'
+import useAxiosInstance from '../Hooks/useAxiosInstance'
+import { useQuery } from '@tanstack/react-query'
 
 const Dashboard = () => {
   let { products } = useAllProducts()
-
   let productsCount = products?.length
+
+  const axiosInstance = useAxiosInstance()
+  const { data: orders, isLoading: isOrdersLoading } = useQuery({
+    queryKey: ['orders'],
+    queryFn: async () => {
+      const response = await axiosInstance.get(`/allOrders`)
+      return response.data
+    }
+  })
 
   return (
     <div className='w-full mt-4 flex justify-between items-center gap-4'>
@@ -18,13 +28,14 @@ const Dashboard = () => {
             <span>Total Products</span>
             <span className='text-[30px] font-bold flex items-center gap-4'>
               <AiFillProduct />
-               {productsCount ?? 0}
+              {productsCount ?? 0}
             </span>
           </div>
           <div className='text-[24px] font-semibold text-[#111] p-6 rounded-xl shadow-lg flex flex-col gap-2'>
             <span>Total Orders</span>
             <span className='text-[30px] font-bold flex items-center gap-4'>
-              <BsBorderStyle /> 20
+              <BsBorderStyle />
+              {orders?.length ?? 0}
             </span>
           </div>
         </div>
