04f1190a716b23db10bb767cd3c9f4480764972e
SajeedAninda <sajaninda@gmail.com>
2025-06-13 16:37:43 +0600

    Created a user route

--- src/app/(UserLayout)/layout.js
+++ src/app/(UserLayout)/layout.js
@@ -1,18 +1,21 @@
+import UserRoute from '@/components/PrivateRoutes/UserRoute';
 import UserSidebar from '@/components/User/UserSidebar';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
 const UserLayout = ({ children }) => {
     return (
-        <div className="flex min-h-screen bg-gradient-to-r from-[#616161] to-[#111111] p-10">
-            <Toaster />
-            <div className="flex w-full">
-                <div className="w-[20%] flex-grow h-full">
-                    <UserSidebar></UserSidebar>
+        <UserRoute>
+            <div className="flex min-h-screen bg-gradient-to-r from-[#616161] to-[#111111] p-10">
+                <Toaster />
+                <div className="flex w-full">
+                    <div className="w-[20%] flex-grow h-full">
+                        <UserSidebar></UserSidebar>
+                    </div>
+                    <div className="w-[80%] h-full">{children}</div>
                 </div>
-                <div className="w-[80%] h-full">{children}</div>
             </div>
-        </div>
+        </UserRoute>
     );
 };
 
---
+++ src/components/PrivateRoutes/UserRoute.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { ColorRing } from 'react-loader-spinner'
+import useCurrentUser from '../Hooks/useCurrentUser'
+import useAuth from '../Hooks/useAuth'
+
+const UserRoute = ({ children }) => {
+  const router = useRouter()
+  const { userData, isUserLoading } = useCurrentUser()
+  const { loggedInUser, loading } = useAuth()
+
+  useEffect(() => {
+    if (!loading && !isUserLoading) {
+      if (!(userData?.role === 'user' && loggedInUser)) {
+        router.replace('/login')
+      }
+    }
+  }, [loading, isUserLoading, userData, loggedInUser, router])
+
+  if (loading || isUserLoading) {
+    return (
+      <div className='flex justify-center min-h-screen items-center'>
+        <ColorRing
+          visible={true}
+          height='80'
+          width='80'
+          ariaLabel='blocks-loading'
+          wrapperStyle={{}}
+          wrapperClass='blocks-wrapper'
+          colors={['#111', '#0e2b45', '#111', '#0e2b45', '#111']}
+        />
+      </div>
+    )
+  }
+
+  if (userData?.role === 'user' && loggedInUser) {
+    return children
+  }
+
+  return null
+}
+
+export default UserRoute
